a4bf3d519f4fa0dc7c854885b99dd47b3a9e0127
Ferit Tunçer <ferit@cryptolab.net>
2020-11-27 03:18:21 +0300

    cleanup(interface): remove unused variables

--- src/ethereum/interface.js
+++ src/ethereum/interface.js
@@ -3,9 +3,6 @@ import web3 from "./web3";
 import DemoToken from "../truffle/build/contracts/DemoToken.json";
 import SimpleBondingCurve from "../truffle/build/contracts/SimpleBondingCurve.json";
 
-const tokenAddress = "0x38EfCFb8d84B9970d344fdB4023F2E94941e3026";
-const address = "0xa3f27ae78A327C2608045C7e5b84703de1a8cE99";
-
 const imports = {
   DemoToken,
   SimpleBondingCurve
